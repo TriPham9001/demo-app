5e92a8794cc14b52d2af5d2c9fb17e2e8ae151e8
TriPham9001 <tri.pham1101@gmail.com>
2022-12-05 16:20:18 +0700

    demo

--- src/components/MyCarousel/MyCarousel.js
+++ src/components/MyCarousel/MyCarousel.js
@@ -7,20 +7,7 @@ function MyCarousel() {
 
   return (
     <>
-      <Carousel>
-        <div>
-          <img src="assets/1.jpeg" />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src="assets/2.jpeg" />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src="assets/3.jpeg" />
-          <p className="legend">Legend 3</p>
-        </div>
-      </Carousel>
+      <h1>Demo</h1>
     </>
   )
 }
